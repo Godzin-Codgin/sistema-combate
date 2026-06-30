297bffc666a1368f895f07c759bc2dd5e1805f1d
Aluno SENAI <Aluno@SN-405089.edu.fiesc.com.br>
2026-06-30 16:26:11 -0300

    feito a luta

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-    // Nosso heroi
+    
     string heroiNome = "Godzin prozin";
     int heroiVida = 120;
     int heroiAtaque = 30;
 
-    // Dois inimigos
+   
     string ini1Nome = "jooji matadorzin";
     int ini1Vida = 70;
     int ini1Ataque = 20;
@@ -22,6 +22,21 @@ int main() {
     mostrarStatus(heroiNome, heroiVida, heroiAtaque);
     mostrarStatus(ini1Nome, ini1Vida, ini1Ataque);
     mostrarStatus(ini2Nome, ini2Vida, ini2Ataque);
+    
+   
+cout << "\n--- O Combate Comeca! ---" << endl;
+
+
+int danoNoIni1 = calcularDano(heroiAtaque, 10);
+ini1Vida -= danoNoIni1;
+cout << heroiNome << " atacou " << ini1Nome << " causando " << danoNoIni1 << " de dano!" << endl;
+mostrarStatus(ini1Nome, ini1Vida, ini1Ataque);
+
+
+int danoNoHeroi = calcularDano(ini2Ataque, 5);
+heroiVida -= danoNoHeroi;
+cout << ini2Nome << " contra-atacou " << heroiNome << " causando " << danoNoHeroi << " de dano!" << endl;
+mostrarStatus(heroiNome, heroiVida, heroiAtaque);
 
     system("pause");
     return 0;
